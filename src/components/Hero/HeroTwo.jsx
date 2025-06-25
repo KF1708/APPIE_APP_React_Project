8cc0c581e77b01e4729fc8acb8da03de4b71fafe
KF1708 <kftech.va@gmail.com>
2025-06-25 16:30:57 -0400

    Fix build path and CSS @import issues

--- src/components/Hero/HeroTwo.jsx
+++ src/components/Hero/HeroTwo.jsx
@@ -2,21 +2,21 @@ const HeroTwo = () => {
   return (
     <div
       data-aos="fade-down"
-      className=" flex-1 hero-image center relative p-10"
+      className="flex-1 hero-image center relative p-10"
     >
       <img
         className="hero-image-circle absolute z-10 scale-125"
-        src="../public/images/solid-circle-png-thumb16.png"
+        src="/images/solid-circle-png-thumb16.png"
         alt="solid-circle"
       />
       <img
         className="hero-image-thumb-1 z-20"
-        src="../public/images/hero-thumb-1.png"
+        src="/images/hero-thumb-1.png"
         alt="hero-thumb-1"
       />
       <img
         className="hero-image-thumb-2 absolute z-30 right-0"
-        src="../public/images/hero-thumb-2.png"
+        src="/images/hero-thumb-2.png"
         alt="hero-thumb-2"
       />
     </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,24 @@
 import { FaChevronDown } from "react-icons/fa";
 import { IoPersonOutline } from "react-icons/io5";
-import { Link } from "react-router";
+import { Link } from "react-router-dom"; // ✅ Correct import
 
 const NavBar = () => {
   return (
     <div className="navbar sticky top-0 z-50 shadow-md container px-12">
       <div className="navbar-section">
         <div className="navbar-half">
-          <img src="../public/images/logo.png" alt="logo" />
+          {/* ✅ Fixed image path */}
+          <img src="/images/logo.png" alt="logo" className="h-10 w-auto" />
+
           <h3 className="makealign">
             Home <FaChevronDown className="icon" />
           </h3>
           <h3 className="makealign">
-            Mega Menu
-            <FaChevronDown className="icon" />
+            Mega Menu <FaChevronDown className="icon" />
           </h3>
           <h3 className="makealign">
             Pages <FaChevronDown className="icon" />
           </h3>
-
           <h3 className="makealign">
             Shop <FaChevronDown className="icon" />
           </h3>
@@ -32,6 +32,7 @@ const NavBar = () => {
             <IoPersonOutline className="icon-p" />
             Login
           </h3>
+
           <Link to="/" style={{ textDecoration: "none" }}>
             <p className="navbar-p">Get Started</p>
           </Link>
